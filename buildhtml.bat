dd18cd317625d53e87b77189b2d1c88cb73e6bc3
Jesse Goodier <31039225+jessegoodier@users.noreply.github.com>
2021-11-10 16:11:07 -0500

    creating module dedicated to API security

--- buildhtml.bat
+++ buildhtml.bat
@@ -1,2 +1,1 @@
 docker run --rm -it -v %cd%:/data -w /data f5devcentral/containthedocs:latest make -C docs html
-docker run --rm -it -v %cd%:/data -w /data f5devcentral/containthedocs:latest make -C docs spelling
