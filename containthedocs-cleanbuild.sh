2410ec40eb8941b8e759302e0280de30d845eca0
Daniel Edgar <aknot242@gmail.com>
2026-01-16 13:50:34 -0500

    explicitly set arch and install python requirements to remove mermaid error

--- containthedocs-cleanbuild.sh
+++ containthedocs-cleanbuild.sh
@@ -2,12 +2,12 @@
 
 set -x
 
-COMMAND="make -C docs clean html"
+COMMAND=(/bin/bash -c "sudo pip install -r requirements.txt -U ; make -C docs clean html")
 
 . ./containthedocs-image
 
-exec docker run --rm -it \
+exec docker run --platform linux/amd64 --rm -it \
   -v "$PWD":"$PWD" --workdir "$PWD" \
   ${DOCKER_RUN_ARGS} \
   -e "LOCAL_USER_ID=$(id -u)" \
-  ${DOC_IMG} ${COMMAND}
+  ${DOC_IMG} "${COMMAND[@]}"
